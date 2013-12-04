ca51a3dc9336c782780fd94dc2747395b15fdc21
Jake Kaufman <Jake@JCK.local>
2013-12-04 15:36:10 -0500

    adding group table view controller and group objects

---
+++ Snapchat/Snapchat/SCGroupTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCGroupTableViewController.h
+//  Snapchat
+//
+//  Created by Jake Kaufman on 12/4/13.
+//  Copyright (c) 2013 Jessica Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCGroupTableViewController : UITableViewController
+@property NSMutableArray *groups;
+@end
