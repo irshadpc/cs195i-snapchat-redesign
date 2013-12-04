ae30f72302c294bbf07e567fb442b91881352cad
jsl15 <jessicasliang@gmail.com>
2013-12-04 15:35:06 -0500

    adding files that forgot to be added

---
+++ Snapchat/Snapchat/SCRequestCell.m
@@ -0,0 +1,29 @@
+//
+//  SCRequestCell.m
+//  Snapchat
+//
+//  Created by Jessica Liang on 12/4/13.
+//  Copyright (c) 2013 Jessica Liang. All rights reserved.
+//
+
+#import "SCRequestCell.h"
+
+@implementation SCRequestCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
