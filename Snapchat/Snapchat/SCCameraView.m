72034d3ea983c91d2b2475d94889d26d85dec524
Jake Kaufman <Jake@JCK.local>
2013-12-03 22:42:51 -0500

    Edited the position of the buttons to align with 4inch iPhone (as opposed to 3.5in)

--- Snapchat/Snapchat/SCCameraView.m
+++ Snapchat/Snapchat/SCCameraView.m
@@ -28,19 +28,20 @@
     self = [super initWithFrame:frame];
     if (self) {
     backCamera = true;
-       // [UIScreen mainScreen].bounds.size.width
+    CGFloat width = [UIScreen mainScreen].bounds.size.width;
+    CGFloat height = [UIScreen mainScreen].bounds.size.height;
     NSLog(@"Width: %f, Height: %f", [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
     UIImage *background_image =  [UIImage imageNamed:@"camera image"];
     self.background_image_view = [[UIImageView alloc] initWithImage:background_image];
-    self.menuButton = [[UIButton alloc] initWithFrame:CGRectMake(20, 430, 30, 30)];
+    self.menuButton = [[UIButton alloc] initWithFrame:CGRectMake(width - 300, height - 50, 30, 30)];
     self.menuButton.backgroundColor = [UIColor redColor];
     [self.menuButton addTarget:self action:@selector(openMenu:) forControlEvents:UIControlEventTouchUpInside];
     
-    self.switchButton = [[UIButton alloc] initWithFrame:CGRectMake(270, 430, 30, 30)];
+    self.switchButton = [[UIButton alloc] initWithFrame:CGRectMake(width - 50, height - 50, 30, 30)];
     self.switchButton.backgroundColor = [UIColor purpleColor];
     [self.switchButton addTarget:self action:@selector(switchCamera:) forControlEvents:UIControlEventTouchUpInside];
     
-    self.snapButton = [[UIButton alloc] initWithFrame:CGRectMake(130, 400, 60, 60)];
+    self.snapButton = [[UIButton alloc] initWithFrame:CGRectMake(width - 190, height - 80, 60, 60)];
     self.snapButton.backgroundColor = [UIColor greenColor];
     [self.snapButton addTarget:self action:@selector(switchCamera:) forControlEvents:UIControlEventTouchUpInside];
 
